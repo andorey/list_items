c4f2be0ee7b1e56943e7cdcc7da6e6f9f91944e3
andoreyrich <andoreyrich@gmail.com>
2019-11-17 19:48:46 +0100

    using spread operatop { ...element } for transmission react elements

--- todo/00-empty-react-project/src/components/todo-list-item.jsx
+++ todo/00-empty-react-project/src/components/todo-list-item.jsx
@@ -1,1 +1,1 @@
-import React from 'react';const TodoListItem = ({label, important}) => {	const spanStyle = {		color: important ? 'tomato' : 'black'	};	return <span style={spanStyle}>{label}</span>};export default TodoListItem;
+import React from 'react';const TodoListItem = ({label, important}) => {	const spanStyle = {		color: important ? 'tomato' : 'black'	};	return <span style={ spanStyle }>{label}</span>};export default TodoListItem;
